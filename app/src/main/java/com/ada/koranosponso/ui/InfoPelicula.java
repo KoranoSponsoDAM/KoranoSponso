487f3391b31d8bd7b0b1b7d25a572d589da872e2
Alejandro <alejandro.b.eec@gmail.com>
2017-05-25 22:11:30 +0200

    Retroceso videos

--- app/src/main/java/com/ada/koranosponso/ui/InfoPelicula.java
+++ app/src/main/java/com/ada/koranosponso/ui/InfoPelicula.java
@@ -1,8 +1,10 @@
 package com.ada.koranosponso.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -20,12 +22,22 @@ public class InfoPelicula extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_pelicula);
         rellenarElementos();
+        agregarToolbar();
     }
 
     public void inicializaElementos(){
         descripcion = (TextView)findViewById(R.id.descripcion);
         titulo = (TextView)findViewById(R.id.txtTitulo);
         imagen = (ImageButton) findViewById(R.id.ibImagen);
+    }
+
+    private void agregarToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     public void rellenarElementos(){
